e2bd3faa1355fe75b2dc4457897931e624868b8e
Tie <125909656+astabrutie8@users.noreply.github.com>
2025-12-15 00:21:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/zodschema/main.go
@@ -0,0 +1,23 @@
+// cmd/zodschema/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "zodschema/internal/zodschema"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := zodschema.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
